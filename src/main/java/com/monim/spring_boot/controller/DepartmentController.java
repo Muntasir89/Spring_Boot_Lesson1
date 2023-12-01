ff136319adb20dd64d7c83e1fdc8e7885cffc0c5
Muntasir Hossen <montachirhossen76@gmail.com>
2023-12-01 23:32:39 +0600

    fetch departmentList route added

--- src/main/java/com/monim/spring_boot/controller/DepartmentController.java
+++ src/main/java/com/monim/spring_boot/controller/DepartmentController.java
@@ -1,12 +1,14 @@
 package com.monim.spring_boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.monim.spring_boot.entity.Department;
 import com.monim.spring_boot.service.DepartmentService;
+import java.util.List;;
 
 @RestController
 public class DepartmentController {
@@ -17,5 +19,10 @@ public class DepartmentController {
     @PostMapping("/department")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/department")
+    public List<Department>  fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
     }
 }
--- src/main/java/com/monim/spring_boot/service/DepartmentService.java
+++ src/main/java/com/monim/spring_boot/service/DepartmentService.java
@@ -1,7 +1,11 @@
 package com.monim.spring_boot.service;
 
+import java.util.List;
+
 import com.monim.spring_boot.entity.Department;
 
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    List<Department> fetchDepartmentList();
 }
